e52960f4282ae2fb51cbbbc887877ecc36d930e8
Andrew Soon <andrewsoonqn@gmail.com>
2026-03-14 21:27:49 +0800

    Add obsidian.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -62,6 +62,8 @@ require('lazy').setup({
   require 'plugins.utils.surround',
   require 'plugins.utils.resolve',
 
+  require 'plugins.extensions.obsidian',
+
   -- The following comments only work if you have downloaded the repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
