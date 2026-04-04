fa62651ffeb724682adf3754859126180bb3e8c3
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-05 00:22:36 +0800

    Add keymaps and change theme

--- lua/plugins/extensions/md-preview.lua
+++ lua/plugins/extensions/md-preview.lua
@@ -5,6 +5,13 @@ return {
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
     vim.g.mkdp_browser = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+    -- Theme & Appearance
+    vim.g.mkdp_theme = 'light' -- Sets overall UI to light
+
+    -- Keymaps
+    vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<cr>', { desc = '[M]arkdown [P]review' })
+    vim.keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop<cr>', { desc = '[M]arkdown Preview [S]top' })
   end,
   ft = { 'markdown' },
 }
