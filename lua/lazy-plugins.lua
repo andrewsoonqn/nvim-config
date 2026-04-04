924bbc45ef942c6c6398e75ee2ceb2fa0e813743
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-05 00:29:39 +0800

    Enable debug and autopairs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -81,10 +81,10 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'plugins.utils.debug',
+  require 'plugins.utils.debug',
   -- require 'plugins.utils.indent_line',
   -- require 'plugins.utils.lint',
-  -- require 'plugins.utils.autopairs',
+  require 'plugins.utils.autopairs',
   -- require 'plugins.utils.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
