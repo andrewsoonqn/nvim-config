d79baff8e9d428d592f0fccb952a10300a1d0ca7
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-14 15:17:03 +0800

    Configure

--- lua/plugins/extensions/obsidian.lua
+++ lua/plugins/extensions/obsidian.lua
@@ -1,5 +1,6 @@
 local function obsidian_new_with_prompt()
   local date = os.date '%d%m%y'
+  -- Get your vault path from the config (or hardcode it if easier)
   local vault_path = "/Users/andrewsoon/Andrew's Vault"
 
   vim.ui.input({ prompt = 'What topic/message? ' }, function(input)
@@ -11,45 +12,44 @@ local function obsidian_new_with_prompt()
     local filename = (input ~= '' and (input .. ' ') or '') .. date .. '.md'
     local full_path = vault_path .. '/' .. filename
 
-    -- 1. Create/Open the file
+    -- 1. Open the file directly (bypass ObsidianNew logic)
     vim.cmd('edit ' .. vim.fn.fnameescape(full_path))
 
-    -- 2. Insert content
+    -- 2. Insert your content into the clean buffer
     vim.api.nvim_buf_set_lines(0, 0, -1, false, {
       '#to-tag',
-      '',
       '# ' .. display_title,
+      '',
       '',
       '',
     })
 
     -- 3. Position cursor
-    vim.api.nvim_win_set_cursor(0, { 5, 0 })
+    vim.api.nvim_win_set_cursor(0, { 4, 0 })
+
+    -- 4. Save the file immediately so Obsidian recognizes it
+    vim.cmd 'write'
   end)
 end
-
 vim.keymap.set('n', '<leader>on', obsidian_new_with_prompt, { desc = '[O]bsidian [N]ote' })
 
 return {
   'epwalsh/obsidian.nvim',
   version = '*',
-  lazy = false,
+  lazy = false, -- Set to false so the keymap works immediately
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  config = function(_, opts)
-    require('obsidian').setup(opts)
-    -- This ensures markdown looks clean (hides syntax) and kills the warning
-    vim.opt.conceallevel = 2
-  end,
   opts = {
     note_id_func = function(title)
       return title
     end,
+
     frontmatter = {
       enabled = false,
     },
     legacy_commands = false,
+
     workspaces = {
       {
         name = "Andrew's Vault",
@@ -62,7 +62,7 @@ return {
       template = '00 STORAGE/Templates/_Dailies Template',
     },
     completion = {
-      nvim_cmp = true,
+      nvim_cmp = false,
       min_chars = 2,
     },
   },
