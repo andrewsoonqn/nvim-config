234368161aa4e761c1e4bbc6c31ecef0dc2d4402
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-14 19:25:53 +0800

    Complete config

--- lua/plugins/utils/snacks.lua
+++ lua/plugins/utils/snacks.lua
@@ -1,17 +1,23 @@
--- lazy.nvim
 return {
   'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
   ---@type snacks.Config
   opts = {
-    indent = {
-      -- your indent configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      animate = {
-        enabled = false,
-      },
-    },
-    bigfile = {},
-    quickfile = {},
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
+    indent = { enabled = true, opts = { animate = false } },
+    input = { enabled = false },
+    picker = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = true },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
   },
 }
