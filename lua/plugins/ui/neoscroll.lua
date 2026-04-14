d1464375695898453f8a01a5e3be8e36cc6c0b8f
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-14 17:11:41 +0800

    Remove C-y keymap conflict

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -1,7 +1,7 @@
 return {
   'karb94/neoscroll.nvim',
   opts = {
-    mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', 'zt', 'zz', 'zb' },
+    mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb' },
     duration_multiplier = 0.5,
   },
 }
