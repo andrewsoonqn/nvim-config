93eddb165ed4b1a0e38b2ecb615f138a73b3d799
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-15 16:09:29 +0800

    Make <C-CR> go to exact path name

--- lua/plugins/search/zoxide.lua
+++ lua/plugins/search/zoxide.lua
@@ -6,27 +6,40 @@ return {
   },
   config = function()
     local z_utils = require 'telescope._extensions.zoxide.utils'
+    local action_state = require 'telescope.actions.state'
 
     require('telescope').setup {
       extensions = {
         zoxide = {
           mappings = {
             default = {
-              -- This is the "Teleport" logic
+              -- Action for selecting an existing zoxide entry
               action = function(selection)
                 require('oil').open(selection.path)
+              end,
+            },
+            -- Add an extra mapping to handle manual input
+            ['<C-cr>'] = { -- Or use '<C-e>' if you prefer
+              action = function(selection)
+                -- Get the raw text currently typed in the prompt
+                local current_input = action_state.get_current_line()
+
+                -- Expand ~ to home directory if present
+                local path = vim.fn.expand(current_input)
+
+                -- Open in Oil
+                require('oil').open(path)
               end,
             },
           },
         },
       },
     }
-    -- Load the extension
+
     require('telescope').load_extension 'zoxide'
 
-    -- Bind the zoxide list to a key
     vim.keymap.set('n', '<leader>cd', function()
       require('telescope').extensions.zoxide.list()
-    end, { desc = 'cd in Oil' })
+    end, { desc = 'Zoxide (Oil)' })
   end,
 }
