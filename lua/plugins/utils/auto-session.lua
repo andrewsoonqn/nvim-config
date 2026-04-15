2e3fa3b981c1ef89d09005b38fc62133ce98ebe2
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-15 23:09:35 +0800

    Save session then quit

--- lua/plugins/utils/auto-session.lua
+++ lua/plugins/utils/auto-session.lua
@@ -7,5 +7,6 @@ return {
     }
 
     vim.keymap.set('n', '<leader>rq', '<cmd>AutoSession restore<CR>', { desc = '[R]estore [Q]uit Session' })
+    vim.keymap.set('n', '<leader>sq', '<cmd>AutoSession save<CR><cmd>qa<CR>', { desc = '[S]ave Session then [Q]uitting' })
   end,
 }
